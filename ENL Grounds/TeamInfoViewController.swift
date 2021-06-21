262b657830aa2694aa41599ec6d591256a20ec79
Daniel Earl <danielearl@me.com>
2021-06-21 13:38:24 +0100

    Remove cocoa pods and google ads from app

--- ENL Grounds/TeamInfoViewController.swift
+++ ENL Grounds/TeamInfoViewController.swift
@@ -7,15 +7,12 @@
 //
 
 import UIKit
-import PersonalizedAdConsent
-import GoogleMobileAds
 
-class TeamInfoViewController: UIViewController, GADBannerViewDelegate {
+class TeamInfoViewController: UIViewController {
 
     @IBOutlet weak var byCar: UITextView!
     @IBOutlet weak var byTrain: UITextView!
     @IBOutlet weak var byDrink: UITextView!
-    @IBOutlet weak var AdViewBox: GADBannerView!
         
     func loadTeamInfo() {
         let carInfo = (parent as! TeamViewController).teamCarInfo
@@ -32,26 +29,7 @@ class TeamInfoViewController: UIViewController, GADBannerViewDelegate {
     
     override func viewDidLoad() {
         loadTeamInfo()
-        AdViewBox.isHidden = true
         super.viewDidLoad()
-        AdViewBox.delegate = self
-        AdViewBox.adUnitID = "ca-app-pub-"
-        AdViewBox.rootViewController = self
-        AdViewBox.load(GADRequest())
         // Do any additional setup after loading the view.
     }
-    
-    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
-        if (AdViewBox.isHidden) {
-            AdViewBox.isHidden = false
-        }
-    }
-    
-    func adView(_ bannerView: GADBannerView,
-                didFailToReceiveAdWithError error: GADRequestError) {
-        NSLog("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
-        AdViewBox.isHidden = true
-    }
 }
-
-
--- Pods/Target Support Files/Pods-ENL Grounds/Pods-ENL Grounds-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ENL_Grounds : NSObject
-@end
-@implementation PodsDummy_Pods_ENL_Grounds
-@end
